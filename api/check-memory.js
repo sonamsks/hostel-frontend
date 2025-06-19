c409237f7e6ba766189a34680d5fa0c51cc06a1d
sonamsks <71845005+sonamsks@users.noreply.github.com>
2025-06-19 16:52:29 +0530

    Update check-memory.js

--- api/check-memory.js
+++ api/check-memory.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   const timestamp = new Date().toISOString();
 
   // Replace this with your actual Google Chat webhook URL
-  const GOOGLE_CHAT_WEBHOOK = "https://chat.googleapis.com/v1/spaces/AAQAf0APmM0/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=2sQGr6oMZSmZ-OtiFhD9_e8SVi1qTorcf94ZClZeCC0";
+  #const GOOGLE_CHAT_WEBHOOK = "https://chat.googleapis.com/v1/spaces/AAQAf0APmM0/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=2sQGr6oMZSmZ-OtiFhD9_e8SVi1qTorcf94ZClZeCC0";
 
   try {
     // Send memory usage alert to Google Chat
